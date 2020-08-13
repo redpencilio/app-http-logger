df47b6f5f943475ee568a907800d48124ab309e4
Gilles Coremans <gilles.coremans@gmail.com>
2020-08-13 15:15:22 +0200

    Add mu-authorization and mu-delta-notifier
    Thes services are added to interact with the new delta functionality in
    docker-network-capture-service.

---
+++ config/delta/rules.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        match: {
+            predicate: { type: "uri", value: "https://w3.org/ns/bde/docker#status" }
+        },
+        callback: {
+            url: "http://capture/.mu/delta", method: "POST"
+        },
+        options: {
+            resourceFormat: "v0.0.1",
+            gracePeriod: 0,
+            ignoreFromSelf: false
+        }
+    }
+];
