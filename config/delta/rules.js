979504dbc05a7b4697ca17726eafa877cb7ed97b
Aad Versteden <madnificent@gmail.com>
2020-10-05 18:25:32 +0200

    PoC of docker stats logging
    This PoC shows Docker stats in the application.  It monitors them and
    stores them in an efficient way.

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -10,6 +10,17 @@ export default [
             resourceFormat: "v0.0.1",
             gracePeriod: 0,
             ignoreFromSelf: false
+        },
+      match: {
+            predicate: { type: "uri", value: "https://w3.org/ns/bde/docker#status" }
+        },
+        callback: {
+            url: "http://stats/.mu/delta", method: "POST"
+        },
+        options: {
+            resourceFormat: "v0.0.1",
+            gracePeriod: 0,
+            ignoreFromSelf: false
         }
     }
 ];
