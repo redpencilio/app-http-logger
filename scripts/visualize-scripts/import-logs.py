993a401245bc2fd64086131624ad9b39246e12c7
Michaël Dierick <michael.dierick@redpencil.io>
2021-07-01 12:35:40 +0200

    reworked to use python gpg client + write decrypted file to disk

--- scripts/visualize-scripts/import-logs.py
+++ scripts/visualize-scripts/import-logs.py
@@ -1,37 +1,42 @@
 #!/bin/env python3
 
-import json
-import requests
 import sys
 import os.path
-import subprocess
+import logging
 import tarfile
 import codecs
+import requests
+import gnupg
 
-# Get JSON events from file
-def get_events(filename, recipient, passphrase_file):
-    # Untar and then read the output
-    if filename.endswith(".json.tar.gz"):
-        tar = tarfile.open(filename)
-        reader = codecs.getreader("utf-8")
-        for member in tar.getmembers():
-            memberfile = tar.extractfile(member)
-            f = reader(memberfile)
-            return [line.rstrip('\n') for line in f]
+GPG_HOME_FOLDER = "/root/.gnupg"
+UNENCRYPTED_LOGS_FOLDER = "/project/data/logs/http"
 
-    else:
-        with open(filename) as f:
-            # Just read the file
-            if filename.endswith(".json"):
-                return [line.rstrip('\n') for line in f]
+def strip_newlines(file):
+    return list(line.rstrip('\n') for line in file)
 
-            # Decrypt and then read the output
-            elif filename.endswith(".json.gpg"):
-                subproc = subprocess.run(["/usr/bin/gpg", "--decrypt", "--recipient", recipient, "--passphrase-file", passphrase_file, "--trust-model", "always", "--batch", "--pinentry-mode", "loopback"],
-                                         stdin=f, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                print(subproc.stderr.decode("utf-8"))
-                subproc.check_returncode()
-                return [line.rstrip('\n') for line in subproc.stdout.decode("utf-8").splitlines()]
+def yield_files_from_filename(file_path, gpg_instance, passphrase):
+    """ Yields file-like objects containing JSON.
+        Since this functions also takes archive files, one filename can potentially yield multiple file-like objects
+    """
+    filename = os.path.split(file_path)[1]
+    if filename.endswith(".json.tar.gz"): # Untar and then read the output
+        with tarfile.open(file_path) as tar:
+            reader = codecs.getreader("utf-8")
+            for member in tar.getmembers():
+                memberfile = tar.extractfile(member)
+                yield reader(memberfile)
+    elif filename.endswith(".json"):
+        with open(file_path) as f:
+            yield f
+    elif filename.endswith(".json.gpg"): # Decrypt and then read the output
+        unencrypted_filename = filename.replace('.json.gpg', '.json')
+        output_path = os.path.join(UNENCRYPTED_LOGS_FOLDER, unencrypted_filename)
+        with open(file_path, "rb") as f:
+            logging.info("Starting decryption for {} ...".format(file_path))
+            decryption_status = gpg_instance.decrypt_file(f, passphrase=passphrase, always_trust=True, output=output_path)
+            if not decryption_status.ok:
+                raise Exception("GPG Decryption failed: {}".format(decryption_status.status))
+            yield yield_files_from_filename(output_path, gpg_instance, passphrase)
 
 # Check input
 if len(sys.argv) < 6:
@@ -52,25 +57,29 @@ index = sys.argv[4]
 command = '{{ "index" : {{ "_index" : "{0:s}" }} }}'.format(index)
 def to_bulk_query(events):
     for event in events:
-        yield command
-        yield event
+        yield command + '\n'
+        yield event + '\n'
+
+logging.info("Initializing Python gpg")
+gpg = gnupg.GPG(gnupghome=GPG_HOME_FOLDER)
 
 # Send input to Bulk API file-per-file
 for filename in sys.argv[5:]:
-    # Get events in file
-    events = get_events(filename, sys.argv[1], sys.argv[2])
+    print("Ingesting file: {0:s}".format(filename))
+    try:
+        for file in yield_files_from_filename(filename, gpg, sys.argv[2]):
+            bulkdata = to_bulk_query(file)
 
-    if events:
-        # Format events for Elasticsearch
-        bulkdata = '\n'.join([line for line in to_bulk_query(events)]) + '\n'
-        bulkdata = bulkdata.encode(encoding='utf-8')
+            # Send request to Bulk API
+            headers = { 'content-type' : 'application/json' }
+            print("Started streaming")
 
-        # Send request to Bulk API
-        headers = { 'content-type' : 'application/json' }
-        r = requests.post("{0:s}/_bulk".format(url), data=bulkdata, headers=headers)
-
-        # Print result
-        print("Ingested file: {0:s}".format(filename))
-        print("Response: {0:d}".format(r.status_code))
-    else:
+            response = requests.post("{0:s}/_bulk".format(url), data=bulkdata, headers=headers, timeout=10)
+            response.raise_for_status()
+            
+            # Print result
+            print("Ingested file: {0:s}".format(filename))
+            print("Response: {0:d}".format(response.status_code))
+    except Exception as e:
         print("Skipped file: {0:s}".format(filename))
+        raise e
