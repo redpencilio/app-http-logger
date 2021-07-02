320c1458535e7cf24a1e678c1490a6cf92dd9fef
Michaël Dierick <michael.dierick@redpencil.io>
2021-07-02 10:26:07 +0200

    remove passphrashe-file since it's no longer needed

--- scripts/visualize-scripts/http.sh
+++ scripts/visualize-scripts/http.sh
@@ -4,13 +4,9 @@ echo -n "Enter key passphrase: "
 read -r -s passphrase
 echo
 
-passphrasefile=$(mktemp)
-echo "$passphrase" > "$passphrasefile"
-
 echo "$passphrase" | gpg --batch --import --pinentry-mode loopback /project/keys/gpg.key
 
 es_index="http-log"
 echo "Going to import logs in Elasticsearch index $es_index"
 
-python3 ./import-logs.py "$1" "$passphrasefile" 'http://elasticsearch:9200' "$es_index" /project/data/encrypted/http/*
-rm "$passphrasefile"
+python3 ./import-logs.py "$1" "$passphrase" 'http://elasticsearch:9200' "$es_index" /project/data/encrypted/http/*
